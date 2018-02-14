8951a21aef89e2cb75b696220346953b5df87487
maxravel <maxmaterniak@gmail.com>
2018-02-14 21:21:47 +0100

    loops

--- js/js.js
+++ js/js.js
@@ -34,13 +34,39 @@
 // };
 
 //scope A
-const myName = () => {
-    //scope B
-    const name = "lara";
-    const herName = () => {
-        //scope C, we can access name here
-        console.log(name);
-    };
-};
+const con = document.querySelector('#con');
+// const name = "lara";
+// const myName = () => {
+//     //scope B
+//     const herName = () => {
+//         //scope C
+//         //console.log(name);
+//         //return name;
+//         con.innerText=name;
+//     };
+//     herName();
+//     // con.innerText=herName();
+// };
+// myName();
 
+//LOOPs
+//while loop
+// let i = 10;
+// let a=[];
+// while(i>1){
+//     a.push(i);
+//     console.log(i);
+//     con.innerText=a;
+//     i-=1;
+// }
+
+//do while loop
+// let i=10
+// do{
+//     i--;
+//     console.log(i);
+// }
+// while(i>0);
+
+// for loop
 
